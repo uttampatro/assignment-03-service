3ef673aca0470bb722d684f587d89efdd4116dde
uttampatro <uttampatro890@gmail.com>
2021-09-15 15:36:46 +0530

    schemas

---
+++ src/entity/Blog.ts
@@ -0,0 +1,27 @@
+import { model, Schema } from 'mongoose';
+
+export interface Blog {
+    title: string;
+    article: string;
+    date: Date;
+}
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        min: 3,
+    },
+    article: {
+        type: String,
+        required: true,
+        max: 255,
+        min: 3,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export default model('blog', blogSchema);
